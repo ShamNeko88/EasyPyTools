a2f1d093ef01bc1f9eb14633242794d58f4ab8a4
Shamneko <mkdvlp01@gmail.com>
2024-11-11 14:03:20 +0000

    本番デプロイ

--- EasyPyTools/settings.py
+++ EasyPyTools/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# アプリ内の静的ファイルをDjangoに認識させる
+STATICFILES_DIRS = [
+    BASE_DIR / "EasyLiveMemo/static",  # EasyLiveMemoアプリのstaticディレクトリ
+    BASE_DIR / "TopPage/static",  # TopPageアプリのstaticディレクトリ
+]
 
 # 本番環境用のSTATIC_ROOTを指定
 STATIC_ROOT = BASE_DIR / "staticfiles"
