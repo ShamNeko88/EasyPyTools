f056d7cb596e0202d6b1ff3875afbed0c49bfc14
ShamNeko <mkdvlp01@gmail.com>
2024-11-14 18:51:49 +0900

    環境変数設定

--- EasyPyTools/settings.py
+++ EasyPyTools/settings.py
@@ -90,11 +90,11 @@ if DJANGO_ENV == 'production':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'ShamNeko88$EasyPyTools_ProdDb',
-            'USER': 'ShamNeko88',
-            'PASSWORD': 'EASYPYTOOLbigf1139?',
-            'HOST': 'ShamNeko88.mysql.pythonanywhere-services.com',
-            'PORT': '3306',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT', '3306'),
         }
     }
 else:
