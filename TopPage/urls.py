72f311372f258dd490643e3d0a041cfd6a515afa
ShamNeko <mkdvlp01@gmail.com>
2024-12-03 11:23:21 +0900

    パスワード変更機能追加

--- TopPage/urls.py
+++ TopPage/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     # 認証関連のURL
     path('login/', auth_views.LoginView.as_view(template_name='TopPage/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='TopPage/password_change.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='TopPage/password_change_done.html'), name='password_change_done'),
 ]
--- TopPage/views.py
+++ TopPage/views.py
@@ -6,6 +6,10 @@ from django.views.generic import (
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import UserRegistrationForm
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
 
 
 # ルート
@@ -26,3 +30,19 @@ class RegisterView(CreateView):
             login(request, user)  # 自動的にログイン
             return redirect('index')  # 登録後のリダイレクト先
         return render(request, 'TopPage/register.html', {'form': form})
+
+
+# パスワード変更
+class PasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    template_name = 'registration/password_change.html'
+    success_url = reverse_lazy('password_change_done')  # 完了ページのURL
+    success_message = "パスワードが変更されました。"
+
+    def form_valid(self, form):
+        messages.success(self.request, self.success_message)
+        return super().form_valid(form)
+
+
+# パスワード変更完了画面
+class PasswordChangeDoneView(LoginRequiredMixin, TemplateView):
+    template_name = 'registration/password_change_done.html'
