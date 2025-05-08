8d36330e4fcdd0bb3cc4c15593665e77deab218c
ShamNeko <mkdvlp01@gmail.com>
2025-05-08 19:24:16 +0900

    テーブル：アンケートヘッダ作成

--- EasySurvey/models.py
+++ EasySurvey/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+# アンケートヘッダ
+class TranSurvey(models.Model):
+    survey_id = models.AutoField(primary_key=True, null=False)  # 主キー
+    title = models.CharField(max_length=100)  # アンケートタイトル
+    detail = models.TextField()  # アンケート詳細
+    created_at = models.DateTimeField(auto_now_add=True)  # 作成日時
+    updated_at = models.DateTimeField(auto_now=True)  # 更新日時
+
+    class Meta:
+        db_table = 'TRN_SURVEY'  # 実際のDBテーブル名を指定
+
+    def __str__(self):
+        return self.title
+
