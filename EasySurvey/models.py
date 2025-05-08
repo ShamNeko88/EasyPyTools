f133bd0ba3e3f9910ba9b7f72b97c6a87aa82c9f
ShamNeko <mkdvlp01@gmail.com>
2025-05-08 20:56:16 +0900

    データベースマイグレーションとりあえず全て終了

--- EasySurvey/models.py
+++ EasySurvey/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 # アンケートヘッダ
-class TranSurvey(models.Model):
-    survey_id = models.AutoField(primary_key=True, null=False)  # 主キー
+class TrnSurvey(models.Model):
+    survey_id = models.AutoField(primary_key=True)  # 主キー
     title = models.CharField(max_length=100)  # アンケートタイトル
-    detail = models.TextField()  # アンケート詳細
+    detail = models.TextField(null=True)  # アンケート詳細
+    access_token = models.CharField(max_length=36)  # アクセストークン
     created_at = models.DateTimeField(auto_now_add=True)  # 作成日時
     updated_at = models.DateTimeField(auto_now=True)  # 更新日時
 
@@ -14,4 +15,35 @@ class TranSurvey(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# アンケート内設問
+class TrnSurveyQuestion(models.Model):
+    survey_id = models.ForeignKey(TrnSurvey, on_delete=models.CASCADE, db_column="survey_id")  # 外部キー
+    question_id = models.AutoField(primary_key=True)  # 主キー
+    question = models.CharField(max_length=100)  # 設問内容
+    created_at = models.DateTimeField(auto_now_add=True)  # 作成日時
+    updated_at = models.DateTimeField(auto_now=True)  # 更新日時
+
+    class Meta:
+        db_table = 'TRN_SURVEY_QUESTION'  # 実際のDBテーブル名を指定
+
+    def __str__(self):
+        return self.survey_id
+
+
+# アンケート回答
+class TrnSurveyAnswer(models.Model):
+    survey_id = models.ForeignKey(TrnSurvey, on_delete=models.CASCADE, db_column="survey_id")  # 外部キー
+    question_id = models.ForeignKey(TrnSurveyQuestion, on_delete=models.CASCADE, db_column="question_id")  # 外部キー
+    answer_id = models.AutoField(primary_key=True)  # 主キー
+    answer = models.CharField(max_length=1)  # 回答内容
+    created_at = models.DateTimeField(auto_now_add=True)  # 作成日時
+    updated_at = models.DateTimeField(auto_now=True)  # 更新日時
+
+    class Meta:
+        db_table = 'TRN_SURVEY_ANSWER'  # 実際のDBテーブル名を指定
+
+    def __str__(self):
+        return self.answer_id
 
