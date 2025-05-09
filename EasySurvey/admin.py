d659a40b25c484bd6d6b7ccb61f5bed7aafb3b80
ShamNeko <mkdvlp01@gmail.com>
2025-05-09 20:53:57 +0900

    [26]管理サイトでデータベース見える所まで実装

--- EasySurvey/admin.py
+++ EasySurvey/admin.py
@@ -1,3 +1,11 @@
+"""管理サイトで管理するための設定を行うファイル
+ここで定義すると、Djangoの管理サイトでモデルを管理できるようになる
+"""
+
 from django.contrib import admin
+from .models import TrnSurvey, TrnSurveyQuestion, TrnSurveyAnswer
 
 # Register your models here.
+admin.site.register(TrnSurvey)
+admin.site.register(TrnSurveyQuestion)
+admin.site.register(TrnSurveyAnswer)
