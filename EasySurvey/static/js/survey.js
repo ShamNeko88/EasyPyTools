182d110f67ab981fac661a494272fd4248ace591
ShamNeko <mkdvlp01@gmail.com>
2025-05-11 01:06:54 +0900

    jsファイル一つで管理している為、DOMがない時にエラってた所を対応

--- EasySurvey/static/js/survey.js
+++ EasySurvey/static/js/survey.js
@@ -23,32 +23,34 @@ document.addEventListener("DOMContentLoaded", function () {
     const questionsContainer = document.getElementById("questions-container");
 
     // 質問追加ボタンがクリックされたときの処理
-    let questionCount = 1; // 現在の質問数をカウントする変数
-    addQuestionBtn.addEventListener("click", function () {
-        questionCount++;
-        // 新しい質問のHTMLを作成
-        const newQuestionDiv = document.createElement("div");
-        // あたらしい質問のdivにクラスを追加（bootstrap）
-        newQuestionDiv.classList.add(
-            "form-group",
-            "d-flex",
-            "align-items-center",
-            "mb-2"
-        );
-        // 新しい質問のHTMLを設定
-        newQuestionDiv.innerHTML = `
-            <input type="text" class="form-control mr-2" id="question${questionCount}" name="questions[]" placeholder="質問を入力" style="flex: 1;">
-            <button type="button" class="btn btn-danger remove-question-btn">削除</button>
-        `;
-        // 質問を追加するコンテナに新しい質問を追加
-        questionsContainer.appendChild(newQuestionDiv);
+    if (addQuestionBtn&&questionsContainer) {
+        let questionCount = 1; // 現在の質問数をカウントする変数
+        addQuestionBtn.addEventListener("click", function () {
+            questionCount++;
+            // 新しい質問のHTMLを作成
+            const newQuestionDiv = document.createElement("div");
+            // あたらしい質問のdivにクラスを追加（bootstrap）
+            newQuestionDiv.classList.add(
+                "form-group",
+                "d-flex",
+                "align-items-center",
+                "mb-2"
+            );
+            // 新しい質問のHTMLを設定
+            newQuestionDiv.innerHTML = `
+                <input type="text" class="form-control mr-2" id="question${questionCount}" name="questions[]" placeholder="質問を入力" style="flex: 1;">
+                <button type="button" class="btn btn-danger remove-question-btn">削除</button>
+            `;
+            // 質問を追加するコンテナに新しい質問を追加
+            questionsContainer.appendChild(newQuestionDiv);
 
-        // 削除ボタンのクリックイベントを追加
-        const removeBtn = newQuestionDiv.querySelector(".remove-question-btn");
-        removeBtn.addEventListener("click", function () {
-            questionsContainer.removeChild(newQuestionDiv);
+            // 削除ボタンのクリックイベントを追加
+            const removeBtn = newQuestionDiv.querySelector(".remove-question-btn");
+            removeBtn.addEventListener("click", function () {
+                questionsContainer.removeChild(newQuestionDiv);
+            });
         });
-    });
+    }
 });
 
 /**
